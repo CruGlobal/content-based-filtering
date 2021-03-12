26c6d000ce253ab42851539b17a39b6bf21d05a3
Scotty Waggoner <ozzieorca@gmail.com>
2021-03-11 18:58:12 -0800

    Add initial S3 code
    - Untested

--- src/s3.ts
+++ src/s3.ts
@@ -1,18 +1,53 @@
+import AWS from 'aws-sdk';
 import { Lockfile, Recommendation } from './types';
 
+const Bucket = process.env.S3_BUCKET_NAME;
+if (!Bucket) {
+  throw new Error('S3_BUCKET_NAME was not set');
+}
+const lockfileKey = 'lockfile.json';
+
+AWS.config.update({ region: 'us-east-1' });
+const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+
 export const putRecommendation = (recommendation: Recommendation) => {
-  console.log('Put', recommendation);
+  return s3
+    .putObject({
+      Bucket,
+      Key: `${recommendation.uri}.json`,
+      Body: recommendation.payload,
+    })
+    .promise();
 };
 export const deleteRecommendation = (recommendation: Recommendation) => {
-  console.log('Delete', recommendation);
+  return s3
+    .deleteObject({
+      Bucket,
+      Key: `${recommendation.uri}.json`,
+    })
+    .promise();
 };
-export const loadLockfile = (): Lockfile => {
+export const loadLockfile = async (): Promise<Lockfile> => {
   console.log('Loading lockfile...');
+  const lockfile = (
+    await s3
+      .getObject({
+        Bucket,
+        Key: lockfileKey,
+      })
+      .promise()
+  ).Body;
   console.log('Lockfile loaded.');
-  return { 'page-url': 1, 'page-2-url': 2 };
+  return (lockfile as Lockfile) ?? {};
 };
-export const saveLockfile = (lockfile: Lockfile) => {
+export const saveLockfile = async (lockfile: Lockfile) => {
   console.info('Saving lockfile...');
-  console.log(lockfile);
+  await s3
+    .putObject({
+      Bucket,
+      Key: lockfileKey,
+      Body: JSON.stringify(lockfile),
+    })
+    .promise();
   console.info('Lockfile saved.');
 };
