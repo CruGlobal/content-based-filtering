7cc3657ff9c8d828dc6fee4b3bfc9ddd665f33c9
Brian Zoetewey <brian.zoetewey@cru.org>
2022-04-19 10:12:51 -0400

    Update for GitHub Actions

--- env.js
+++ env.js
@@ -1,15 +1,9 @@
-'use strict'
-
+// This file is used by serverless to load environment from .env, and to set environment fields in functions
 module.exports = () => {
-  // Use dotenv to load local development overrides
-  require('dotenv').config()
-  return {
-    // keys defined here get added to the lambda functions environments
-    ENVIRONMENT: process.env.ENVIRONMENT || 'development',
-    PROJECT_NAME: process.env.PROJECT_NAME || 'content-based-filtering',
-    ROLLBAR_ACCESS_TOKEN: process.env.ROLLBAR_ACCESS_TOKEN || 'secret',
-    GOOGLE_APPLICATION_CREDENTIALS: process.env.GOOGLE_APPLICATION_CREDENTIALS || 'keyfile.json',
-    S3_BUCKET_NAME: process.env.S3_BUCKET_NAME || '',
-    BIGQUERY_TABLE_NAME: process.env.BIGQUERY_TABLE_NAME || ''
+  const { parsed } = require('dotenv').config()
+  const env = {}
+  for (const [key, value] of Object.entries(parsed)) {
+    env[key] = process.env[key] || value
   }
+  return env
 }
